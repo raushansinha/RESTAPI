92a2a72c9b15e878b613e3996ae850f5a6d059b3
Raushan Sinha <hr.raushan@gmail.com>
2017-09-26 00:02:58 -0400

    Added Controller

---
+++ Library/src/Library.API/Controllers/AuthorsControllers.cs
@@ -0,0 +1,44 @@
+﻿using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Helpers;
+
+namespace Library.API.Controllers
+{
+  [Route("api/authors")]
+  public class AuthorsControllers : Controller
+  {
+    private ILibraryRepository _libraryRepository;
+
+    public AuthorsControllers(ILibraryRepository libraryRepository)
+    {
+      _libraryRepository = libraryRepository;
+    }
+
+    [HttpGet()]
+    public IActionResult GetAuthors()
+    {
+      var authorsFromRepo = _libraryRepository.GetAuthors();
+
+      var authors = new List<AuthorDto>();
+
+      foreach (var author in authorsFromRepo)
+      {
+        authors.Add(new AuthorDto()
+        {
+          Id = author.Id,
+          Name = $"{author.FirstName} {author.LastName}",
+          Genre = author.Genre,
+          age = author.DateOfBirth.GetCurrentAge()
+        });
+      }
+      
+      return new JsonResult(authors);
+    }
+  }
+}
